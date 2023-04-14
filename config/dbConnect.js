d6a78275de3b797c7726c7736fed8ed8932515c8
Asliddin0807 <114002286+Asliddin0807@users.noreply.github.com>
2023-04-14 18:09:23 +0500

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const Dbconnect = async () => {
     mongoose.set("strictQuery",false)
-    await mongoose.connect("mongodb://127.0.0.1:27017/quiz")
+    await mongoose.connect("mongodb+srv://ASl:asliddin123@cluster0.vid3jua.mongodb.net/?retryWrites=true&w=majority")
     .then(()=>{
         console.log("connect to mongodb");
     })
